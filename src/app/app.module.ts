40ff467941d5e22ca6116dacfba70db6e9e02c64
roxaline <mariadelarosita078@gmail.com>
2019-01-28 17:04:14 +0200

    add create a component called details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoxyComponent } from './roxy/roxy.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoxyComponent
+    RoxyComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
