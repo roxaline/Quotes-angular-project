a1c9895481e0cb04591cc4cbea95347fe1003b8c
roxaline <mariadelarosita078@gmail.com>
2019-01-28 18:51:35 +0200

    import elements from app.component.html

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,8 @@
 export class Quotes {
+    // public quote: string;
+    // public author: string;
+    constructor(public quote:string, public author:string){
+        this.quote=quote;
+        this.author=author;
+    }
 }
