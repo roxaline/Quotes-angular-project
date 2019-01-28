0f375a936c60536e301764c8f4e801608f7f86dd
roxaline <mariadelarosita078@gmail.com>
2019-01-28 18:50:47 +0200

    comment some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+//   quotes = [
+//     new Quotes('A word to the wise is sufficient', 'Dickens'),
+//     new Quotes('Oh what a tangled web we weave when first we practice to deceive.', 'Sir Walter Scott'),
+//     new Quotes('Examine what is said, not him who speaks.', 'Arabian proverb' ),
+//     new Quotes('I not only use all the brains I have, but all that I can borrow.', 'Woodrow Wilson' ),
+// ]
+
 }
