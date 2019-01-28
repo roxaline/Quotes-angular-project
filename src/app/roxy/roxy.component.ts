b47c3cb66e7d68f04387fab73915259e1b1746e0
roxaline <mariadelarosita078@gmail.com>
2019-01-28 18:55:10 +0200

    add import some elements

--- src/app/roxy/roxy.component.ts
+++ src/app/roxy/roxy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../quotes'
 
 @Component({
   selector: 'app-roxy',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roxy.component.css']
 })
 export class RoxyComponent implements OnInit {
-
+  quotes = [
+    new Quotes('A word to the wise is sufficient', 'Dickens'),
+    new Quotes('Oh what a tangled web we weave when first we practice to deceive.', 'Sir Walter Scott'),
+    new Quotes('Examine what is said, not him who speaks.', 'Arabian proverb' ),
+    new Quotes('I not only use all the brains I have, but all that I can borrow.', 'Woodrow Wilson' ),
+]
   constructor() { }
 
   ngOnInit() {
