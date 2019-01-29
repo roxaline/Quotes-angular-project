ab21ab207252bbbe0c64eb2e1f3f833d2103ee76
roxaline <mariadelarosita078@gmail.com>
2019-01-29 12:39:38 +0200

    add an EventEmmiter

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {quotes} from '../quotes'
 
 @Component({
@@ -8,7 +8,12 @@ import {quotes} from '../quotes'
 })
 export class DetailsComponent implements OnInit {
 
-  @Input() quotes:quotes;
+  @Input() quote:quotes;
+  @Output() isComplete= new EventEmitter<boolean>();
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+
+  }
   constructor() { }
 
   ngOnInit() {
