c8b351419494c4ffb38a11ca5f9bfb2affbf4ba0
roxaline <mariadelarosita078@gmail.com>
2019-01-29 10:54:40 +0200

    add public description

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,8 +1,9 @@
-export class Quotes {
+export class quotes {
     // public quote: string;
     // public author: string;
-    constructor(public quote:string, public author:string){
+    constructor(public quote:string, public author:string, public description:string){
         this.quote=quote;
         this.author=author;
+        this.description=description;
     }
 }
