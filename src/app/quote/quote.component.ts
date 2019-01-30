4277438a222ab0bd1c51a0ef4a2db5f633fdf46c
roxaline <mariadelarosita078@gmail.com>
2019-01-30 13:28:39 +0200

    add styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,10 +7,10 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1,'A word to the wise is sufficient','Created by', 'Dickens',""),
-    new Quote(2,'Oh what a tangled web we weave when first we practice to deceive.','Created by','Sir Walter Scott',""),
-    new Quote(3,'Examine what is said, not him who speaks.','Created by','Arabian proverb',""),
-    new Quote(4,'I not only use all the brains I have, but all that I can borrow.','Created by','Woodrow Wilson',"")
+    new Quote(1,'A word to the wise is sufficient','Created by', 'Dickens',new Date(2019,1,1)),
+    new Quote(2,'Oh what a tangled web we weave when first we practice to deceive.','Created by','Sir Walter Scott',new Date(2019,1,1)),
+    new Quote(3,'Examine what is said, not him who speaks.','Created by','Arabian proverb',new Date(2019,1,1)),
+    new Quote(4,'I not only use all the brains I have, but all that I can borrow.','Created by','Woodrow Wilson',new Date(2019,1,1))
 ];
   toogleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
